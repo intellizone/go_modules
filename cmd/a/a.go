8958ef431b0fcdef0302782a0b23ed0a0180f667
Suriya Prakash <suriyaprakash@moonshades.in>
2025-09-19 14:34:02 +0000

    Greetings

--- cmd/a/a.go
+++ cmd/a/a.go
@@ -1,7 +1,9 @@
 package main
 
 import "github.com/intellizone/go_modules/pkg/hello"
+import "github.com/intellizone/pkg/greetings"
 
 func main() {
 	hello.Hello()
+	greetings.Hello("Suriya Prakash")
 }
